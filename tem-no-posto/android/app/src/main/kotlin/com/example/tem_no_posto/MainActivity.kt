e06890dca33e00d1e410d0b12daa0888257ccf59
Pedro Gabriel Prazeres <pedrogprazeres@gmail.com>
2023-04-07 13:15:44 -0300

    commit initial

---
+++ tem-no-posto/android/app/src/main/kotlin/com/example/tem_no_posto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tem_no_posto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tem-no-posto/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'TEM NO POSTO',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("image/sus.png"),
+                            fit: BoxFit.cover
+                      ),
+                    ), 
+              ),
+              Container(
+                width: 200,
+                height: 320,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("image/tijucas.png"),
+                            fit: BoxFit.cover
+                      ),
+                    ), 
+              ),
+            ],
+          ),
+        ],
+      ),
+
+
+
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   children: [
+      //     Row(
+      //       children: [
+      //        Center(
+      //         child: Container(
+      //             width: 200.0,
+      //             height: 200.0,
+      //             margin: EdgeInsets.all(20),
+                  // decoration: BoxDecoration(
+                  // image: DecorationImage(
+                  //     image: AssetImage("image/healthcare.png"),
+                  //           fit: BoxFit.cover
+                  //     ),
+                  //   ), 
+                  // ),
+      //       ),
+      //         Container(
+      //           width: 200,
+      //           height: 200,
+      //           padding: EdgeInsets.all(70),
+      //           margin: EdgeInsets.all(20),
+      //           decoration: BoxDecoration(
+      //             image: DecorationImage(
+      //               image: AssetImage("image/tijucas.png"),
+      //                 fit: BoxFit.cover,
+      //               ),
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ],
+      // ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //     items: [ 
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.lightbulb),
+      //         label: 'Como usar o Aplicativo?',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.info),
+      //         label: 'Informações',
+      //       ),
+      //     ],
+      //   ),
+      );
+  }
+}
